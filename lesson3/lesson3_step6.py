e395ef8c7e3a559c025cb0757391469777897e9d
albinapotapova <albinkakam26@gmail.com>
2021-09-14 22:50:39 +0300

    new folder

---
+++ lesson3/lesson3_step6.py
@@ -0,0 +1,39 @@
+from selenium import webdriver
+import time
+import math
+
+try:
+    link = "http://suninjuly.github.io/redirect_accept.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    # Нажимаем кнопку Submit.
+    button = browser.find_element_by_css_selector("[type='submit']")
+    button.click()
+    
+    # Переходим на новую вкладку
+    new_window = browser.window_handles[1]
+    browser.switch_to.window(new_window)
+    
+    # Находим значение х на странице и вычисляем функцию
+    x_element = browser.find_element_by_id('input_value')
+    x = x_element.text
+
+    def calc(x):
+        return str(math.log(abs(12 * math.sin(int(x)))))
+
+    y = calc(x)
+
+    # Вводим ответ в поле
+    answer = browser.find_element_by_id('answer')
+    answer.send_keys(y)    
+
+    # Нажимаем кнопку Submit.
+    button = browser.find_element_by_css_selector("[type='submit']")
+    button.click()
+    
+finally:
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(10)
+    # закрываем браузер после всех манипуляций
+    browser.quit()

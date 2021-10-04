e6cc24912fbb7b85aa4a36355301e589167d4953
albinapotapova <albinkakam26@gmail.com>
2021-10-04 23:56:35 +0300

    corrections were made according to the assignment

--- language_test/conftest.py
+++ language_test/conftest.py
@@ -1,23 +1,25 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 def pytest_addoption(parser):
-    parser.addoption('--language', action='store', default="en-gb",
-                 help="Choose language: en-gb, ru, es..")
-
+    parser.addoption('--language', action='store', default=None,
+                 help="Choose language: ru, es, en-gb..")
 
 @pytest.fixture(scope="function")
 def browser(request):
-    browser_name = request.config.getoption("browser_name")
-    browser = None
-    if browser_name == "chrome":
-        print("\nstart chrome browser for test..")
-        browser = webdriver.Chrome()
-    elif browser_name == "firefox":
-        print("\nstart firefox browser for test..")
-        browser = webdriver.Firefox()
-    else:
-        raise pytest.UsageError("--browser_name should be chrome or firefox")
+    language = request.config.getoption("language")
+    
+    options = Options()
+    options.add_experimental_option('prefs', {'intl.accept_languages': language})    
+
+    print("\nstart chrome browser for test..")
+    browser = webdriver.Chrome(options=options)
+    
     yield browser
     print("\nquit browser..")
     browser.quit()
+
+
+
+       
--- language_test/test_items.py
+++ language_test/test_items.py
@@ -1,5 +1,12 @@
-link = "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/"
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_different_interface_language(browser):
-    browser.get(link)
-    browser.find_element_by_css_selector('[class="add-to-basket"]')
+    try:
+        browser.get(link)
+        browser.implicitly_wait(10)
+        browser.find_element_by_css_selector('[class="add-to-basket"]')
+        return True
+    except:
+        return False    
+    assert test_different_interface_language(
+        browser) == True, "Корзина не найдена"
